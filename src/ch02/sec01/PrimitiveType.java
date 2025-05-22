5387d4c497df3c7c888e23287562bcfd99413e64
hyeon3233 <yeonho3233@naver.com>
2025-05-22 18:03:11 +0900

    진수 공부

---
+++ src/ch02/sec01/PrimitiveType.java
@@ -0,0 +1,25 @@
+package ch02.sec01;
+
+public class PrimitiveType {
+    public static void main(String[] args) {
+        /*
+        기본(원시) 타입 (Primitive Type) - 총 8개
+        정수: byte, short, int, long
+        실수: float, double
+        문자: char
+        논리: boolean
+
+        모두 타입명이 소문자로 구성
+
+        23 (10진수) > 10111 (2진수)
+        10111 (2진수) >
+
+        16 (10진수) > 10000
+        1010101 (2진수) > (8진수) 027
+
+        55(10진수) > (8진수) 067
+
+
+         */
+    }
+}
